6d1e47e396744084c0c923c29df0b46f3e329573
fabrice-fouron <fouronfp05@gmail.com>
2022-03-31 08:59:15 -0400

    trying hard

--- client.py
+++ client.py
@@ -3,6 +3,7 @@ import threading
 from time import sleep
 from tkinter import *
 import sys
+import queue
 
 
 '''
@@ -33,52 +34,52 @@ root.geometry('425x500')
 root.title("CHATROOM")
 root.resizable(width=True, height=True)
 
+msg_list = []
 
+client = Client("Client 1")
+client.connect(('localhost', 5005))
 
-
-frameTop = Frame(root, highlightbackground="blue", highlightthickness=2)
+frameTop = Text(root, bg="#ABB2B9")
 frameTop.pack(side=TOP, padx=20, pady=20)
 
-frameBottom = Frame(root)
-frameBottom.pack(side=BOTTOM)
+labelBottom = Label(root, bg="#ABB2B9")
+labelBottom.place(relheight=1)
 
-send_entry = Entry(frameBottom)
+send_entry = Entry(root)
 send_entry.pack()
+
+scrollb = Scrollbar(frameTop)
+scrollb.place(relheight=1, relx=0.974)
+scrollb.config(command=frameTop.yview)
+
+# frameTop.config(state=DISABLED)
 
 
 def send_msg():
     temp = send_entry.get()
+    client.send(temp)
     send_entry.delete(0, END)
+    frameTop.insert(END, msg_list.pop(0))
 
 
-
-send_button = Button(frameBottom, text="Send", command=send_msg)
+send_button = Button(root, text="Send", command=send_msg)
 send_button.pack(side=RIGHT)
 
 
 def add():
     '''Add new message to the frame'''
-    norm = Label(frameTop, text="hello", font='courier')
+    norm = Label(frameTop, text="hello\n\n", font='courier')
     norm.pack()
 
 
-def client_1():
-    try:
-        client = Client("Client 1")
-        client.connect(('localhost', 5005))
-    except Exception:
-        print("PROBLEM CONNECTING TO THE SERVER... TRY AGAIN LATER")
-        sys.exit()
-
-
-x = threading.Thread(target=client_1)
-
-
 def main():
-    x.start()
-
     while True:
         root.update()
+        tempo = client.receive(1024)
+        msg_list.append(tempo)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception:
+        print("APP CLOSED")
